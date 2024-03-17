40f28aaaaf52b1c3c471f5946b656102a93f5988
matyii <kmatyas2004@gmail.com>
2024-03-17 18:38:20 +0100

    test3

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,18 +5,18 @@
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => 'b4269de8048a708bf1d125c50967a0c22c3b2583',
-        'name' => '__root__',
+        'reference' => 'dcbd27e7c33e130a372308e619851ffd264faaef',
+        'name' => 'matyii/molnarcukraszda.hu-v2',
         'dev' => true,
     ),
     'versions' => array(
-        '__root__' => array(
+        'matyii/molnarcukraszda.hu-v2' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => 'b4269de8048a708bf1d125c50967a0c22c3b2583',
+            'reference' => 'dcbd27e7c33e130a372308e619851ffd264faaef',
             'dev_requirement' => false,
         ),
     ),
