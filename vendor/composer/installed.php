d00389a87feb4e0bea0f57a729ab63e816790823
matyii <kmatyas2004@gmail.com>
2024-03-17 18:34:42 +0100

    test

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b4269de8048a708bf1d125c50967a0c22c3b2583',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b4269de8048a708bf1d125c50967a0c22c3b2583',
+            'dev_requirement' => false,
+        ),
+    ),
+);
